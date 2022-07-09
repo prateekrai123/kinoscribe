39bd58a8b3d8378545a3c08ab9d2acd21e389929
prateekrai123 <rprateek254@gmail.com>
2022-07-09 08:47:00 +0530

    update

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,22 +1,36 @@
-import React from 'react'
+import React from "react";
 // import MainHeader from '../../Components/MainHeader/MainHeader';
-import './HomePage.css';
-import HeroImage from '../../Images/Project_69-08.jpg'
+import "./HomePage.css";
+import MainImage from "../../Images/logo.png";
 const HomePage = () => {
   return (
     <div>
-    <div class="row">
-      <div class="column left">
-        <h1>India’s most active student community with 80,000+ members.</h1>
-        <p>Learn from experts and network through events, join clubs and learn real life skills, Hang out on our discord server to make new friends and have fun!</p>
-      </div>
-      <div class="column right">
-        <img alt="" src={HeroImage}/>
+      <div class="row">
+        <div class="column left">
+          <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero autem
+            dolore atque eos, culpa commodi unde inventore deleniti sit
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero autem
+            dolore atque eos, culpa commodi unde inventore deleniti sit
+            veritatis, voluptatibus numquam rerum suscipit enim, perspiciatis
+            pariatur doloribus? At consequatur sunt voluptatem molestiae fugit
+            id illo consequuntur rem tempora mollitia impedit libero, magnam
+            inventore accusantium! Natus sunt ab voluptatibus consequuntur
+            distinctio veniam quos praesentium labore? Ipsum, vitae totam.
+            Dignissimos, modi. Iure, minus quis. Soluta eveniet recusandae
+            cupiditate sunt asperiores voluptas molestias ipsam fugit nulla
+            accusamus repellat, ratione odio nesciunt repellendus quas
+            accusantium quis maiores unde, corporis sequi blanditiis fuga quos?
+          </p>
+        </div>
+        <div class="column right">
+          <img alt="" src={MainImage} />
+        </div>
       </div>
     </div>
-    </div>
-   
-  )
-}
+  );
+};
 
 export default HomePage;
